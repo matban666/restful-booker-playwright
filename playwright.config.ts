943819ac6e9de567ed04f86b9d975e4fccc852f0
Matt Banbury <matt.banbury@sysmech.co.uk>
2025-01-26 16:32:12 +0000

    Trying production url

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8080/',
-    // baseURL: 'https://automationintesting.online/',
+    // baseURL: 'http://localhost:8080/',
+    baseURL: 'https://automationintesting.online/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
